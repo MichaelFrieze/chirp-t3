fc16432fb9520ff6408127ad61330a0b826b78d7
Michael Frieze <michaelfrieze@icloud.com>
2023-04-01 22:27:09 -0400

    cleaned up the SinglePostPage

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,137 +1,7 @@
-import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Image from "next/image";
 import Head from "next/head";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-import { LoadingPage, LoadingSpinner } from "~/components/loading";
-
-import { api, type RouterOutputs } from "~/utils/api";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-
-dayjs.extend(relativeTime);
-
-const CreatePostWizard = () => {
-  const { user } = useUser();
-
-  const [input, setInput] = useState("");
-
-  const ctx = api.useContext();
-
-  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
-    onSuccess: () => {
-      setInput("");
-      void ctx.posts.getAll.invalidate();
-    },
-    onError: (e) => {
-      const errorMessage = e.data?.zodError?.fieldErrors.content;
-      if (errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0]);
-      } else {
-        toast.error("Failed to post! Please try again later.");
-      }
-    },
-  });
-
-  console.log(user);
-
-  if (!user) return null;
-
-  return (
-    <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <input
-        placeholder="Type some emojis!"
-        className="grow bg-transparent outline-none"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            if (input !== "") {
-              mutate({ content: input });
-            }
-          }
-        }}
-        disabled={isPosting}
-      />
-      {input !== "" && !isPosting && (
-        <button onClick={() => mutate({ content: input })}>Post</button>
-      )}
-      {isPosting && (
-        <div className="flex items-center justify-center">
-          <LoadingSpinner size={20} />
-        </div>
-      )}
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  // You can use &nbsp; to create a non breaking space
-  // or put span's in a <pre></pre>
-  // or apply white-space: pre; in CSS (whitespace-pre in tailwind)
-
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        alt={`@${author.username} profile picture`}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex whitespace-pre text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">{` · ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
-        </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
-    </div>
-  );
-};
-
-const Feed = () => {
-  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
-
-  if (postsLoading) return <LoadingPage />;
-
-  if (!data) return <div>Something went wrong</div>;
-
-  return (
-    <div className="flex flex-col">
-      {data.map((fullPost) => (
-        <PostView {...fullPost} key={fullPost.post.id} />
-      ))}
-    </div>
-  );
-};
 
 const SinglePostPage: NextPage = () => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
-
-  // Start fetching asap
-  // with react-query, it caches data so you only have to call it once
-  api.posts.getAll.useQuery();
-
-  // Return empty div if user isn't loaded
-  if (!userLoaded) return <div />;
-
   return (
     <>
       <Head>
@@ -140,7 +10,7 @@ const SinglePostPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen justify-center">
-        <div>Post View</div>
+        <div>Profile View</div>
       </main>
     </>
   );
