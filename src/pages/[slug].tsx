f71852bdbb4ee587086073422967f5b11610f895
Michael Frieze <michaelfrieze@icloud.com>
2023-04-02 17:58:42 -0400

    using tRPC createProxySSGHelpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,27 +1,55 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 
-const ProfilePage: NextPage = () => {
-  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "michaelfrieze",
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
   });
-  if (isLoading) return <div>Loading...</div>;
-
   if (!data) return <div>404</div>;
-
-  console.log(data);
 
   return (
     <>
       <Head>
-        <title>Chirp | Profile</title>
+        <title>{`Chirp | ${username}`}</title>
       </Head>
       <main className="flex h-screen justify-center">
         <div>{data.username}</div>
       </main>
     </>
   );
+};
+
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default ProfilePage;
